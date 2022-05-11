91b2a81da5acdc204015109861ef708f380affae
Matthijs <matthijs07g@gmail.com>
2022-05-11 21:33:52 +0200

    misschien fix

--- test/integration/user.controller.test.js
+++ test/integration/user.controller.test.js
@@ -12,7 +12,7 @@ describe("Manage users", () => {
       database = [];
       done();
     });
-    it("Verplicht veld ontbreekt, een gepaste error word terug gestuurd", (done) => {
+    it("TC-201-1 Verplicht veld ontbreekt, een gepaste error word terug gestuurd", (done) => {
       chai
         .request(server)
         .post("/api/user")
@@ -31,6 +31,92 @@ describe("Manage users", () => {
           result.should.be
             .a("string")
             .that.equals("Emailadress must be a string");
+          done();
+        });
+    });
+    // it("TC-201-2 Niet-valide email adres", (done) => {
+    //   chai
+    //     .request(server)
+    //     .post("/api/user")
+    //     .send({
+    //       firstName: "Mark",
+    //       lastName: "Doe",
+    //       street: "Lovensdijkstraat 61",
+    //       city: "Breda",
+    //       password: "secret",
+    //       //emailAdress ontbreekt
+    //     })
+    //     .end((err, res) => {
+    //       res.should.be.an("object");
+    //       let { status, result } = res.body;
+    //       status.should.equals(400);
+    //       result.should.be
+    //         .a("string")
+    //         .that.equals("Emailadress must be a string");
+    //       done();
+    //     });
+    // });
+    // it("TC-201-3 Niet-valide wachtwoord", (done) => {
+    //   chai
+    //     .request(server)
+    //     .post("/api/user")
+    //     .send({
+    //       firstName: "Mark",
+    //       lastName: "Doe",
+    //       street: "Lovensdijkstraat 61",
+    //       city: "Breda",
+    //       password: "secret",
+    //       //emailAdress ontbreekt
+    //     })
+    //     .end((err, res) => {
+    //       res.should.be.an("object");
+    //       let { status, result } = res.body;
+    //       status.should.equals(400);
+    //       result.should.be
+    //         .a("string")
+    //         .that.equals("Emailadress must be a string");
+    //       done();
+    //     });
+    // });
+    // it("TC-201-4 Gebruiker bestaat al", (done) => {
+    //   chai
+    //     .request(server)
+    //     .post("/api/user")
+    //     .send({
+    //       firstName: "Mark",
+    //       lastName: "Doe",
+    //       street: "Lovensdijkstraat 61",
+    //       city: "Breda",
+    //       password: "secret",
+    //       //emailAdress ontbreekt
+    //     })
+    //     .end((err, res) => {
+    //       res.should.be.an("object");
+    //       let { status, result } = res.body;
+    //       status.should.equals(400);
+    //       result.should.be
+    //         .a("string")
+    //         .that.equals("Emailadress must be a string");
+    //       done();
+    //     });
+    // });
+    it("TC-201-5 Gebruiker succesvol geregistreerd", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          firstName: "Mark",
+          lastName: "Doe",
+          street: "Lovensdijkstraat 61",
+          city: "Breda",
+          password: "secret",
+          emailAdress: "j.doe@server.com",
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(201);
+          result.should.be.an("array");
           done();
         });
     });
