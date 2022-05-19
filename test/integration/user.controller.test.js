8c3cf4e3a7a58d1124d0f7082c7189693469500d
Matthijs <matthijs07g@gmail.com>
2022-05-19 14:20:26 +0200

    test

--- test/integration/user.controller.test.js
+++ test/integration/user.controller.test.js
@@ -55,42 +55,42 @@ describe("Manage users", () => {
         });
     });
   });
-  describe("UC-203 Gebruikersprofiel opvragen /api/user/:Id", () => {
-    beforeEach((done) => {
-      database = [];
-      done();
-    });
-    it("TC-203-1 Ongeldige token, een gepaste error word terug gestuurd", (done) => {
-      chai.request(server).get("/api/user/:Id");
-      const Id = req.params.Id;
-      console.log(`User met ID ${Id} gezocht`);
-      let user = database
-        .filter((item) => item.id == Id)
-        .send({ Id })
-        .end((err, res) => {
-          res.should.be.an("object");
-          let { status, result } = res.body;
-          status.should.equals(401);
-          result.should.be
-            .a("string")
-            .that.equals("User with ID" + Id + "not found");
-          done();
-        });
-    });
-    it("TC-203-2 Valide token en gebruiker bestaat, een gepaste error word terug gestuurd", (done) => {
-      chai.request(server).get("/api/user/:Id");
-      const Id = req.params.Id;
-      console.log(`User met ID ${Id} gezocht`);
-      let user = database
-        .filter((item) => item.id == Id)
-        .send({ Id })
-        .end((err, res) => {
-          res.should.be.an("object");
-          let { status, result } = res.body;
-          status.should.equals(200);
-          result.should.be.an("array").that.equals(user);
-          done();
-        });
-    });
-  });
+  // describe("UC-203 Gebruikersprofiel opvragen /api/user/:Id", () => {
+  //   beforeEach((done) => {
+  //     database = [];
+  //     done();
+  //   });
+  //   it("TC-203-1 Ongeldige token, een gepaste error word terug gestuurd", (done) => {
+  //     chai.request(server).get("/api/user/:Id");
+  //     const Id = req.params.Id;
+  //     console.log(`User met ID ${Id} gezocht`);
+  //     let user = database
+  //       .filter((item) => item.id == Id)
+  //       .send({ Id })
+  //       .end((err, res) => {
+  //         res.should.be.an("object");
+  //         let { status, result } = res.body;
+  //         status.should.equals(401);
+  //         result.should.be
+  //           .a("string")
+  //           .that.equals("User with ID" + Id + "not found");
+  //         done();
+  //       });
+  //   });
+  //   it("TC-203-2 Valide token en gebruiker bestaat, een gepaste error word terug gestuurd", (done) => {
+  //     chai.request(server).get("/api/user/:Id");
+  //     const Id = req.params.Id;
+  //     console.log(`User met ID ${Id} gezocht`);
+  //     let user = database
+  //       .filter((item) => item.id == Id)
+  //       .send({ Id })
+  //       .end((err, res) => {
+  //         res.should.be.an("object");
+  //         let { status, result } = res.body;
+  //         status.should.equals(200);
+  //         result.should.be.an("array").that.equals(user);
+  //         done();
+  //       });
+  //   });
+  // });
 });
