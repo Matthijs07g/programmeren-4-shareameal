0d01be2d1977e5fd43597b6b206e21a3fc21dedc
Matthijs07g <matthijs07g@gmail.com>
2022-07-03 12:21:09 +0200

    user tests bijna klaar

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,15 +2,18 @@ const express = require("express");
 const { getUser } = require("../contollers/user.controller");
 const router = express.Router();
 const userController = require("../contollers/user.controller");
+const authController  = require("../contollers/auth.controller")
 
-router.post("/api/user", userController.validateUser, userController.addUser);
+router.post("/api/user", authController.validateToken, userController.validateUser, userController.addUser);
 
-router.get("/api/user/:Id", userController.getUser);
+router.get("/api/user/:Id", authController.validateToken, userController.getUser);
 
-router.get("/api/user", userController.getAllUsers);
+router.get("/api/user", authController.validateToken, userController.getAllUsers);
 
-router.put("/api/user/:Id", userController.validateUser, userController.putUser);
+router.get("/api/user/profile", authController.validateToken, userController.getUserProfile)
 
-router.delete("/api/user/:Id", userController.deleteUser);
+router.put("/api/user/:Id", authController.validateToken, userController.validateUser, userController.putUser);
+
+router.delete("/api/user/:Id", authController.validateToken, userController.deleteUser);
 
 module.exports = router;
